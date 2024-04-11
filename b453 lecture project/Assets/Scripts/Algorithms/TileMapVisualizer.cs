99e12f85984dea4548879394dbf093441e15235f
rubroshe <rubroshe@iu.edu>
2024-04-10 23:38:02 -0400

    Procedural Generation

---
+++ b453 lecture project/Assets/Scripts/Algorithms/TileMapVisualizer.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class TileMapVisualizer : MonoBehaviour
+{
+    [SerializeField]
+    private Tilemap floorTileMap;
+
+    [SerializeField]    
+    private TileBase floorTile;
+
+    public void PaintFloorTiles(IEnumerable<Vector2Int> floorPositions)
+    {
+        PaintTiles(floorPositions, floorTileMap, floorTile);
+    }
+
+    private void PaintTiles(IEnumerable<Vector2Int> positions, Tilemap tilemap, TileBase tile)
+    {
+        foreach (var position in positions)
+        {
+            PaintSingleTile(tilemap, tile, position);
+        }
+    }
+
+    private void PaintSingleTile(Tilemap tilemap, TileBase tile, Vector2Int position)
+    {
+        var tilePosition = tilemap.WorldToCell((Vector3Int)position);
+        tilemap.SetTile(tilePosition, tile);
+    }
+
+    public void Clear()
+    {
+        floorTileMap.ClearAllTiles();
+    }
+}
